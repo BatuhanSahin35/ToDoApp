26e4f2eca8c2a9d298982ee242b316f4ca1c0946
Batuhan Sahin <batuhan.sahin@ceng.deu.edu.tr>
2022-09-27 16:52:56 +0300

    BackUp

---
+++ notionclone.entity/TemplateList.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace notionclone.entity
+{
+    public class TemplateList
+    {
+        public int Id { get; set; }
+        public string UserId { get; set; }
+    }
+}
